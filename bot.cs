b580ec023c56bbbcc78310fc85b39cd3ce9a5d1d
jamesenprofit <jamesenprofit@live.de>
2022-06-29 22:13:37 +0200

    Add files via upload

---
+++ bot.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSockets;
+
+namespace Bot.Commands
+{
+    class Program
+    {
+        static void
+        {
+            
+        }
+    }
+}
